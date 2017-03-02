412914116f069860ff9a931b0f063ec8166197b1
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2017-03-02 17:31:12 +0900

    fix bug, add race: npm new-thunk 0.0.2

---
+++ work/new-thunk2-test.js
@@ -0,0 +1,35 @@
+void function () {
+	'use struct';
+
+	//const Thunk = require('new-thunk/thunk2');
+	const Thunk = require('../thunk2');
+	const aa = Thunk.aa;
+
+	function delay(ms, val) {
+		console.log('delay', ms, val);
+		return Thunk(function (cb) {
+			setTimeout(function (err, val) {
+				console.log('delay', ms, val, 'END');
+				cb(err, val);
+			}, ms, null, val);
+		});
+	}
+
+	delay(500, 'a')
+	((err, val) => err ? err : delay(500, 'b'))
+	((err, val) => err ? err : delay(500, 'c'))
+	((err, val) => err ? err : delay(500, 'd'))
+	((err, val) => err ? console.error(err) : null);
+
+	setTimeout(aa(function *() {
+		console.log('aa start');
+		yield delay(500, 'd0');
+		console.log(yield Thunk.all([delay(200, 'd1'), delay(100, 'd2'), delay(300, 'd3')]));
+		console.log(yield Thunk.all({x:delay(200, 'd1'), y:delay(100, 'd2'), z:delay(300, 'd3')}));
+		console.log(yield []);
+		console.log(yield {});
+		console.log(yield Thunk.race([delay(200, 'd1'), delay(100, 'd2'), delay(300, 'd3')]));
+		console.log(yield Thunk.race({x:delay(200, 'd1'), y:delay(100, 'd2'), z:delay(300, 'd3')}));
+	}), 2500, function () { console.log('aa end'); });
+
+} ();
